08dfe8e0cdd690fc58ae8079d0f536756a38a3c9
Steve Lhomme <slhomme@levelupstudio.com>
2013-02-16 19:00:41 +0100

    basic Plume plugin for DashClock using the unread broadcast from Plume

---
+++ src/com/levelup/touiteur/dashclock/DashClockService.java
@@ -0,0 +1,41 @@
+package com.levelup.touiteur.dashclock;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.text.TextUtils;
+
+import com.google.android.apps.dashclock.api.DashClockExtension;
+import com.google.android.apps.dashclock.api.ExtensionData;
+
+
+public class DashClockService extends DashClockExtension {
+
+	@Override
+	protected void onUpdateData(int reason) {
+		SharedPreferences storage = StatusReceiver.getStorage(getApplicationContext());
+		ExtensionData updatedData = new ExtensionData();
+		updatedData.icon(R.drawable.widget_picholder);
+		updatedData.visible(true);
+		updatedData.clickIntent(new Intent().setClassName("com.levelup.touiteur", "com.levelup.touiteur.TouiteurMain"));
+		
+		int unreadTweets = storage.getInt(StatusReceiver.INTENT_UNREAD_TWEET, 0);
+		int unreadMentions = storage.getInt(StatusReceiver.INTENT_UNREAD_MENTION, 0);
+		int unreadDMs = storage.getInt(StatusReceiver.INTENT_UNREAD_DM, 0);
+		
+		updatedData.status(String.format("%d unread", (unreadTweets + unreadMentions + unreadDMs)));
+		
+		updatedData.expandedTitle(String.format("Tweets %d / Mentions %d / DMs %d", unreadTweets, unreadMentions, unreadDMs));
+		String user = storage.getString(StatusReceiver.INTENT_UNREAD_USER, null);
+		String notifText = storage.getString(StatusReceiver.INTENT_UNREAD_TEXT, null);
+		StringBuilder expandedBody = new StringBuilder();
+		if (!TextUtils.isEmpty(user)) {
+			expandedBody.append(user);
+			expandedBody.append(": ");
+		}
+		if (!TextUtils.isEmpty(notifText)) {
+			expandedBody.append(notifText);
+		}
+		updatedData.expandedBody(expandedBody.toString());
+		publishUpdate(updatedData);
+	}
+
+}
---
+++ src/com/levelup/touiteur/dashclock/StatusReceiver.java
@@ -0,0 +1,44 @@
+package com.levelup.touiteur.dashclock;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor;
+import android.util.Log;
+
+
+public class StatusReceiver extends BroadcastReceiver {
+	private static final String GLOBAL_NOTIF = "com.levelup.touiteur.action.GLOBALNOTIF";
+	static final String INTENT_UNREAD_TWEET   = "UnreadT";
+	static final String INTENT_UNREAD_MENTION = "UnreadM";
+	static final String INTENT_UNREAD_DM      = "UnreadD";
+	static final String INTENT_UNREAD_TEXT    = "UnreadText";
+	static final String INTENT_UNREAD_USER    = "User";
+	
+	private static final String PERSISTANT_STORAGE = "storage";
+
+	static final String LOG_TAG = "PlumeDashClock";
+	
+	@Override
+	public void onReceive(Context context, Intent intent) {
+		if (intent!=null && GLOBAL_NOTIF.equals(intent.getAction())) {
+			// we received the broadcast status from Plume
+			//Log.wtf(LOG_TAG, "received Plume notification "+intent.getExtras());
+			
+			SharedPreferences storage = getStorage(context);
+			Editor e = storage.edit();
+			e.putInt(INTENT_UNREAD_TWEET,   intent.getIntExtra(INTENT_UNREAD_TWEET, 0));
+			e.putInt(INTENT_UNREAD_MENTION, intent.getIntExtra(INTENT_UNREAD_MENTION, 0));
+			e.putInt(INTENT_UNREAD_DM,      intent.getIntExtra(INTENT_UNREAD_DM, 0));
+			e.putString(INTENT_UNREAD_TEXT, intent.getStringExtra(INTENT_UNREAD_TEXT));
+			e.putString(INTENT_UNREAD_USER, intent.getStringExtra(INTENT_UNREAD_USER));
+			e.commit();
+		}
+	}
+	
+	public static SharedPreferences getStorage(Context context) {
+		return context.getSharedPreferences(PERSISTANT_STORAGE, 0);
+		
+	}
+
+}
