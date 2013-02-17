3d89856eee537bb3bd992f766be2ba1c7f81a651
Steve Lhomme <slhomme@levelupstudio.com>
2013-02-17 16:28:36 +0100

    propose to download Plume if it's not installed

--- src/com/levelup/touiteur/dashclock/DashClockSettings.java
+++ src/com/levelup/touiteur/dashclock/DashClockSettings.java
@@ -1,10 +1,16 @@
 package com.levelup.touiteur.dashclock;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.preference.PreferenceActivity;
 import android.view.MenuItem;
 
 public class DashClockSettings extends PreferenceActivity {
+
+	private static final String PLUME_PACKAGE = "com.levelup.touiteur";
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +24,37 @@ public class DashClockSettings extends PreferenceActivity {
 		super.onPostCreate(savedInstanceState);
 		addPreferencesFromResource(R.xml.preferences);
 	}
+	
+	@Override
+	protected void onResume() {
+		super.onResume();
+		boolean isPlumeInstalled = false;
+		try {
+			Intent plumeInfo = getPackageManager().getLaunchIntentForPackage("com.levelup.touiteur");
+			isPlumeInstalled = plumeInfo!=null;
+		} catch (Throwable e) {
+		}
+		if (!isPlumeInstalled) {
+			AlertDialog.Builder warnMissing = new AlertDialog.Builder(this);
+			warnMissing.setTitle(android.R.string.dialog_alert_title);
+			warnMissing.setMessage(R.string.warn_plume_missing);
+			warnMissing.setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
+				@Override
+				public void onClick(DialogInterface dialog, int which) {
+					Intent marketIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("market://details?id="+PLUME_PACKAGE));
+					startActivity(marketIntent);
+				}
+			});
+			warnMissing.setOnCancelListener(new DialogInterface.OnCancelListener() {
+				@Override
+				public void onCancel(DialogInterface dialog) {
+					finish();
+				}
+			});
+			warnMissing.show();
+		}
+	}
+	
 
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
