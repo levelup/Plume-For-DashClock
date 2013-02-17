9bf853211e24fe05eed42e5d3d6b25369192415b
Steve Lhomme <slhomme@levelupstudio.com>
2013-02-17 14:52:54 +0100

    add a setting to show/hide the widget when all counters are 0 (hide by default)

---
+++ src/com/levelup/touiteur/dashclock/DashClockSettings.java
@@ -0,0 +1,30 @@
+package com.levelup.touiteur.dashclock;
+
+import android.os.Bundle;
+import android.preference.PreferenceActivity;
+import android.view.MenuItem;
+
+public class DashClockSettings extends PreferenceActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		getActionBar().setDisplayHomeAsUpEnabled(true);
+	}
+	
+	@Override
+	protected void onPostCreate(Bundle savedInstanceState) {
+		super.onPostCreate(savedInstanceState);
+		addPreferencesFromResource(R.xml.preferences);
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            finish();
+            return true;
+        }
+		return super.onOptionsItemSelected(item);
+	}
+	
+}
