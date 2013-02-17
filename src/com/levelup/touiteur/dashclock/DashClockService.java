421fdf8842590134301af69884da16ba588716e1
Steve Lhomme <slhomme@levelupstudio.com>
2013-02-17 15:58:54 +0100

    add configuration to tell which kind of counter should be shown or not (TL, Mention, DM)

--- src/com/levelup/touiteur/dashclock/DashClockService.java
+++ src/com/levelup/touiteur/dashclock/DashClockService.java
@@ -9,7 +9,12 @@ import com.google.android.apps.dashclock.api.ExtensionData;
 
 public class DashClockService extends DashClockExtension {
 	
-	private static final String USER_PREFS = "preferences";
+	static final String USER_PREFS = "preferences";
+	
+	private static final String PREF_HIDE_EMPTY = "HideWhenEmpty";
+	private static final String PREF_SHOW_TWEET = "ShowCounterTweet";
+	private static final String PREF_SHOW_MENTION = "ShowCounterMentions";
+	private static final String PREF_SHOW_DM = "ShowCounterDMs";
 
 	@Override
 	protected void onInitialize(boolean isReconnect) {
@@ -19,24 +24,54 @@ public class DashClockService extends DashClockExtension {
 
 	@Override
 	protected void onUpdateData(int reason) {
+		SharedPreferences userPrefs = getApplicationContext().getSharedPreferences(USER_PREFS, 0);
+		
 		SharedPreferences storage = StatusReceiver.getStorage(getApplicationContext());
-		int unreadTweets = storage.getInt(StatusReceiver.INTENT_UNREAD_TWEET, 0);
-		int unreadMentions = storage.getInt(StatusReceiver.INTENT_UNREAD_MENTION, 0);
-		int unreadDMs = storage.getInt(StatusReceiver.INTENT_UNREAD_DM, 0);
+		Integer unreadTweets = !userPrefs.getBoolean(PREF_SHOW_TWEET, true) ? null : storage.getInt(StatusReceiver.INTENT_UNREAD_TWEET, 0);
+		Integer unreadMentions = !userPrefs.getBoolean(PREF_SHOW_MENTION, true) ? null : storage.getInt(StatusReceiver.INTENT_UNREAD_MENTION, 0);
+		Integer unreadDMs = !userPrefs.getBoolean(PREF_SHOW_DM, true) ? null : storage.getInt(StatusReceiver.INTENT_UNREAD_DM, 0);
 
+		//Log.d(StatusReceiver.LOG_TAG, "update "+unreadTweets+" / "+unreadMentions+" / "+unreadDMs+" hide:"+userPrefs.getBoolean(PREF_HIDE_EMPTY, true));
 		ExtensionData updatedData;
-		if (getApplicationContext().getSharedPreferences(USER_PREFS, 0).getBoolean("HideWhenEmpty", true)
-				&& unreadTweets==0 && unreadMentions==0 && unreadDMs==0)
+		if (userPrefs.getBoolean(PREF_HIDE_EMPTY, true)
+				&& (unreadTweets==null   || unreadTweets==0)
+				&& (unreadMentions==null || unreadMentions==0)
+				&& (unreadDMs==null      || unreadDMs==0)) {
 			updatedData = null; // reset so it not displayed
-		else {
+		} else {
 			updatedData = new ExtensionData();
+			updatedData.visible(true);
 			updatedData.icon(R.drawable.ic_launcher);
 			updatedData.clickIntent(new Intent().setClassName("com.levelup.touiteur", "com.levelup.touiteur.TouiteurMain"));
+			
+			StringBuilder status = new StringBuilder();
+			if (unreadTweets!=null) {
+				status.append(String.valueOf(unreadTweets));
+			}
+			if (unreadMentions!=null) {
+				if (status.length()!=0) status.append('/');
+				status.append(String.valueOf(unreadMentions));
+			}
+			if (unreadDMs!=null) {
+				if (status.length()!=0) status.append('/');
+				status.append(String.valueOf(unreadDMs));
+			}
+			updatedData.status(status.toString());
 
-			updatedData.visible(unreadTweets!=0 || unreadMentions!=0 || unreadDMs!=0);
-			updatedData.status(getString(R.string.status, unreadTweets, unreadMentions, unreadDMs));
-
-			updatedData.expandedTitle(getString(R.string.expanded_title, unreadTweets, unreadMentions, unreadDMs));
+			StringBuilder expanded = new StringBuilder();
+			if (unreadTweets!=null) {
+				expanded.append(getString(R.string.expanded_title_tweet, unreadTweets));
+			}
+			if (unreadMentions!=null) {
+				if (expanded.length()!=0) expanded.append(" / ");
+				expanded.append(getString(R.string.expanded_title_mention, unreadMentions));
+			}
+			if (unreadDMs!=null) {
+				if (expanded.length()!=0) expanded.append(" / ");
+				expanded.append(getString(R.string.expanded_title_dm, unreadDMs));
+			}
+			updatedData.expandedTitle(expanded.toString());
+			
 			String user = storage.getString(StatusReceiver.INTENT_UNREAD_USER, null);
 			String notifText = storage.getString(StatusReceiver.INTENT_UNREAD_TEXT, null);
 			StringBuilder expandedBody = new StringBuilder();
