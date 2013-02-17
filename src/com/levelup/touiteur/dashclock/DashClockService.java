201e393f9e53f1eb7aa18455b1427ad86b3cc544
Steve Lhomme <slhomme@levelupstudio.com>
2013-02-17 14:27:35 +0100

    by default don't show anything when counters are empty

--- src/com/levelup/touiteur/dashclock/DashClockService.java
+++ src/com/levelup/touiteur/dashclock/DashClockService.java
@@ -8,6 +8,8 @@ import com.google.android.apps.dashclock.api.ExtensionData;
 
 
 public class DashClockService extends DashClockExtension {
+	
+	private static final String USER_PREFS = "preferences";
 
 	@Override
 	protected void onInitialize(boolean isReconnect) {
@@ -21,26 +23,32 @@ public class DashClockService extends DashClockExtension {
 		int unreadTweets = storage.getInt(StatusReceiver.INTENT_UNREAD_TWEET, 0);
 		int unreadMentions = storage.getInt(StatusReceiver.INTENT_UNREAD_MENTION, 0);
 		int unreadDMs = storage.getInt(StatusReceiver.INTENT_UNREAD_DM, 0);
-		
-		ExtensionData updatedData = new ExtensionData();
-		updatedData.icon(R.drawable.widget_picholder);
-		updatedData.clickIntent(new Intent().setClassName("com.levelup.touiteur", "com.levelup.touiteur.TouiteurMain"));
-		
-		updatedData.visible(unreadTweets!=0 || unreadMentions!=0 || unreadDMs!=0);
-		updatedData.status(getString(R.string.status, unreadTweets, unreadMentions, unreadDMs));
-		
-		updatedData.expandedTitle(getString(R.string.expanded_title, unreadTweets, unreadMentions, unreadDMs));
-		String user = storage.getString(StatusReceiver.INTENT_UNREAD_USER, null);
-		String notifText = storage.getString(StatusReceiver.INTENT_UNREAD_TEXT, null);
-		StringBuilder expandedBody = new StringBuilder();
-		if (!TextUtils.isEmpty(user)) {
-			expandedBody.append(user);
-			expandedBody.append(": ");
+
+		ExtensionData updatedData;
+		if (getApplicationContext().getSharedPreferences(USER_PREFS, 0).getBoolean("HideWhenEmpty", true)
+				&& unreadTweets==0 && unreadMentions==0 && unreadDMs==0)
+			updatedData = null; // reset so it not displayed
+		else {
+			updatedData = new ExtensionData();
+			updatedData.icon(R.drawable.widget_picholder);
+			updatedData.clickIntent(new Intent().setClassName("com.levelup.touiteur", "com.levelup.touiteur.TouiteurMain"));
+
+			updatedData.visible(unreadTweets!=0 || unreadMentions!=0 || unreadDMs!=0);
+			updatedData.status(getString(R.string.status, unreadTweets, unreadMentions, unreadDMs));
+
+			updatedData.expandedTitle(getString(R.string.expanded_title, unreadTweets, unreadMentions, unreadDMs));
+			String user = storage.getString(StatusReceiver.INTENT_UNREAD_USER, null);
+			String notifText = storage.getString(StatusReceiver.INTENT_UNREAD_TEXT, null);
+			StringBuilder expandedBody = new StringBuilder();
+			if (!TextUtils.isEmpty(user)) {
+				expandedBody.append(user);
+				expandedBody.append(": ");
+			}
+			if (!TextUtils.isEmpty(notifText)) {
+				expandedBody.append(notifText);
+			}
+			updatedData.expandedBody(expandedBody.toString());
 		}
-		if (!TextUtils.isEmpty(notifText)) {
-			expandedBody.append(notifText);
-		}
-		updatedData.expandedBody(expandedBody.toString());
 		publishUpdate(updatedData);
 	}
 }
